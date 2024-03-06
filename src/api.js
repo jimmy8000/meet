309b4fc467e88fbbd0b8cc830bab9bb07cbbd73c
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-03-06 17:21:49 -0600

    4.9: Progressive Web Applications V-2

--- src/api.js
+++ src/api.js
@@ -85,6 +85,7 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
+    NProgress.done();
     return events?JSON.parse(events):[];
   }
 
@@ -96,6 +97,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
