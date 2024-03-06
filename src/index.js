cadfea621caa0ab3e1d51797b810bbfbc37851f2
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-03-06 11:17:44 -0600

    4.7: Continuous Delivery V1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('4e36089f959a44ed9ae2b01d26ad7d59').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
