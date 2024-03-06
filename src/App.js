d18b4e10241faed6a38ce9004f3f66e4291b4084
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2024-03-06 17:36:07 -0600

    4.9: Progressive Web Applications Task

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents, extractLocations } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -13,6 +13,7 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,6 +25,11 @@ const App = () => {
       setEvents(eventsToShow);
       setAllLocations(extractLocations(allEvents));
     };
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("You are offline. Events may not be up-to-date.");
+    }
 
     fetchData();
   }, [currentCity, currentNOE]);
@@ -33,6 +39,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
       </div>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
       <NumberOfEvents eventNumber={currentNOE} setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
